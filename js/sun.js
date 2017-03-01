5b03c023a5fea6832006091bbd0b554ca28d0d1e
lucerio <lucero.develops@gmail.com>
2017-03-01 07:34:28 -0500

    added water and sun.js working on setting up the site so most information in stored in js files

---
+++ js/sun.js
@@ -0,0 +1,10 @@
+
+var HTMLbioPic = '<img src="%data%" class="biopic">';
+
+var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+
+
+var HTMLprojectStart = '<div class="project-entry"></div>';
+var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectDescription = '<p><br>%data%</p>';
+var HTMLprojectImage = '<img src="%data%">';
