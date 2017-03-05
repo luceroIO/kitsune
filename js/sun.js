60a681b7784c1d0444cad7ace599ba2a347db300
lucerio <lucero.develops@gmail.com>
2017-03-05 16:25:08 -0500

    basic mobile design done , needs some style now

--- js/sun.js
+++ js/sun.js
@@ -1,10 +1,13 @@
 
-var HTMLbioPic = '<img src="%data%" class="biopic">';
+var HTMLbioPic = '<img class="img-responsive" src="%data%" alt="bio pic">';
 
-var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+var HTMLwelcomeMsg = '<span>%data%</span>';
 
 
+
+/*
 var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
+*/
